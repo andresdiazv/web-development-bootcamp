0856eeeba25868c8db0261b84d08ed9f50fc4d0d
andresdiazv <adiaz616@fiu.edu>
2023-02-02 10:45:35 -0500

    finished small weather api website

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,16 +1,46 @@
 const express = require("express");
 const { get } = require("http");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const apiKey = "{API KEY}";
+  const unit = "imperial";
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=abce4665d98310aea880d497debdc07d";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    unit;
+    
   https.get(url, function (response) {
     console.log(response.statusCode);
+
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write(
+        "<h1>The temperature in " + query + " is " + temp + " degrees fahrenheit.</h1>"
+      );
+      res.write("<img src=" + imageURL + ">");
+      res.send();
+    });
   });
-  res.send("Server is up and running");
 });
 
 app.listen(3000, function () {
