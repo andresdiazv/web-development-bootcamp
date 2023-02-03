4f6090e2bd005905babdd8fe4f922d63a7bc7b02
andresdiazv <adiaz616@fiu.edu>
2023-02-02 19:20:05 -0500

    finished connecting to mailchimp

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,27 +1,60 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const mailchimp = require('@mailchimp/mailchimp_marketing');
+ 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-
+ 
+mailchimp.setConfig({
+  apiKey: "API_KEY",
+  server: "SERVER_PREFIX"
+});
+ 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/signup.html");
-    });
-
+  res.sendFile(__dirname + "/signup.html");
+})
+ 
 app.post("/", function(req, res){
-    var firstname = req.body.fName;
-    var lastname = req.body.lName;
-    var email = req.body.email;
-    
-    console.log(firstname, lastname, email);
-    });
-
-    
+  console.log(req.body.firstName);
+  console.log(req.body.lastName);
+  console.log(req.body.email);
+ 
+  const listId = "LIST_ID";
+  const subscribingUser = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email
+  };
+ 
+  async function run() {
+      try {
+          const response = await mailchimp.lists.addListMember(listId, {
+            email_address: subscribingUser.email,
+            status: "subscribed",
+            merge_fields: {
+              FNAME: subscribingUser.firstName,
+              LNAME: subscribingUser.lastName
+            }
+          });
+ 
+          console.log(
+            `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+          );
+ 
+          res.sendFile(__dirname + "/success.html");
+      } catch (e) {
+          res.sendFile(__dirname + "/failure.html");
+      }
+  }
+ 
+  run();
+})
+ 
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+})
+ 
 app.listen(3000, function () {
-    console.log("Server is running on port 3000.");
-    });
-
-
+  console.log("Server is running on port 3000")
+});
